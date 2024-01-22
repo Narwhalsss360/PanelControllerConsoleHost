d1be56a8ca6af9a7a45dee65812dce7e1e9875bc
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-01-22 03:41:44 -0500

    start

--- ConsoleHost/Program.cs
+++ ConsoleHost/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using CLIApplication;
+using PanelController.Controller;
+
+CLIInterpreter interpreter = new();
+
+#region Back API
+#endregion
+
+#region Commands
+#region General
+void Clear()
+{
+    Console.Clear();
+}
+
+void Quit()
+{
+    Main.Deinitialize();
+}
+#endregion
+#endregion
+
+Main.Initialize();
+interpreter.Run(Main.DeinitializedCancellationToken);
