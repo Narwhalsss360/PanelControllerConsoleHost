9b78a5129b65d81022c9af9090f796c14edb28bf
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-01-22 12:10:35 -0500

    Added ShowLoadedExtensions

--- ConsoleHost/Program.cs
+++ ConsoleHost/Program.cs
@@ -1,13 +1,29 @@
 ﻿using CLIApplication;
 using PanelController.Controller;
+using PanelController.PanelObjects.Properties;
+using System.Net.Http.Headers;
 
-CLIInterpreter interpreter = new();
+CLIInterpreter interpreter = new(ShowLoadedExtensions, Clear, Quit);
 
 #region Back API
 #endregion
 
 #region Commands
 #region General
+void ShowLoadedExtensions(Extensions.ExtensionCategories? category = null)
+{
+    if (category is null)
+    {
+        foreach (var item in Enum.GetValues<Extensions.ExtensionCategories>())
+            ShowLoadedExtensions(item);
+        return;
+    }
+
+    Console.WriteLine($"{category.Value}:");
+    foreach (var item in Extensions.ExtensionsByCategory[category.Value])
+        Console.WriteLine($"{item.GetItemName()}({item.FullName}):{item.GetItemDescription()}");
+}
+
 void Clear()
 {
     Console.Clear();
