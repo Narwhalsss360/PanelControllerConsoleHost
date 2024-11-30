f0df80549c97249020734ecfaf425712be73aaea
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-01 01:59:37 +0200

    Implemented Named Pipe

--- ConsoleExtensions/Channels/NamedPipe.cs
+++ ConsoleExtensions/Channels/NamedPipe.cs
@@ -4,6 +4,40 @@ using System.IO.Pipes;
 
 namespace ConsoleExtensions.Channels
 {
+    public static class PipeStreamExtension
+    {
+        public static byte[] ReadNextMessage(this PipeStream pipe, int timeoutMilliseconds = 1)
+        {
+            if (pipe.ReadMode != PipeTransmissionMode.Message)
+            {
+                throw new Exception($"{ReadNextMessage} requires pipe read mode to be Message");
+            }
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            byte[] firstByteBuffer = new byte[1];
+            Task readTask = pipe.ReadAsync(firstByteBuffer, 0, 1, cts.Token);
+            bool timeout = Task.WaitAny(readTask, Task.Delay(timeoutMilliseconds)) == 1;
+
+            if (readTask.IsFaulted && readTask.Exception is not null)
+            {
+                throw readTask.Exception;
+            }
+
+            if (timeout)
+            {
+                return new byte[0];
+            }
+
+            List<byte> message = new List<byte>() { firstByteBuffer[0] };
+            while (!pipe.IsMessageComplete)
+            {
+                message.Add((byte)pipe.ReadByte());
+            }
+
+            return message.ToArray();
+        }
+    }
+
     [ItemName("Named Pipe")]
     public class NamedPipe : IChannel
     {
@@ -11,24 +45,66 @@ namespace ConsoleExtensions.Channels
 
         public event EventHandler<byte[]>? BytesReceived;
 
-        public NamedPipe()
+        private NamedPipeServerStream _pipe;
+
+        private Thread? _readerThread = null;
+
+        public NamedPipe(string pipeName)
         {
-            throw new NotImplementedException("Incomplete");
+            _pipe = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.None, 512, 512);
+            
+        }
+
+        private void ReaderThread()
+        {
+            while (_pipe.IsConnected)
+            {
+                byte[] data = _pipe.ReadNextMessage();
+                if (data.Length == 0)
+                {
+                    Thread.Sleep(1);
+                }
+                BytesReceived?.Invoke(this, data);
+            }
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            if (_pipe.IsConnected)
+            {
+                _pipe.Close();
+            }
+
+            if (_readerThread is not null)
+            {
+                _readerThread.Join();
+                _readerThread = null;
+            }
+            
         }
 
         public object? Open()
         {
-            throw new NotImplementedException();
+            if (_pipe.IsConnected)
+            {
+                return null;
+            }
+
+            if (_readerThread is not null)
+            {
+                return new Exception($"Program error: {nameof(_readerThread)} is not null in ${nameof(Open)}");
+            }
+
+            _pipe.WaitForConnection();
+            _readerThread = new Thread(ReaderThread);
+            _readerThread.Start();
+            return null;
         }
 
         public object? Send(byte[] data)
         {
-            throw new NotImplementedException();
+            _pipe.Write(data, 0, data.Length);
+            return null;
         }
     }
 }
